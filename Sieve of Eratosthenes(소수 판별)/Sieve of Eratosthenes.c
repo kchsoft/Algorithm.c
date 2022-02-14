d3b51296e719766854765fa5664fa27b3868f8c3
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-02-14 10:51:04 +0900

    Eratosthenes 버그 수정
    1. MaxNumber가 <=가 아닌 < 이다.

--- Sieve of Eratosthenes(소수 판별)/Sieve of Eratosthenes.c
+++ Sieve of Eratosthenes(소수 판별)/Sieve of Eratosthenes.c
@@ -3,9 +3,9 @@
 
 int* Sieve_of_Eratosthenes(int* arr,int MaxNumber) {
 
-    for (int i = 2; i * i <= MaxNumber; i++)
+    for (int i = 2; i * i < MaxNumber; i++)
     {
-        for (int k = i; i * k <= MaxNumber; k++)
+        for (int k = i; i * k < MaxNumber; k++)
         {
             arr[i * k] = 0;
         }
