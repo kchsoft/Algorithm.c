03756335ad34ef954b3d0c1271a8c9ca6a5f2898
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-07-02 18:28:27 +0900

    isPrime.c 수정

--- Prime(소수 판별)/isPrime.c
+++ Prime(소수 판별)/isPrime.c
@@ -1,7 +1,7 @@
 #include<stdlib.h>
 
 bool isPrime(int Number) {
-	for (int i = 2; i * i < Number; i++)
+	for (int i = 2; i * i <= Number; i++)
 		if (Number % i * i == 0) return false;
 
 	return true;
